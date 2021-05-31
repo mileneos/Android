cdf160dbbacb41b84d634c634e797eeefd49a37f
mileneos <mileneos@users.noreply.github.com>
2021-05-31 15:01:07 +0300

    1-6

---
+++ PracticeA6/room/src/main/java/ru/mirea/zernov/room/AppDatabase.java
@@ -0,0 +1,9 @@
+package ru.mirea.zernov.room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {MainActivity.Employee.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract MainActivity.EmployeeDao employeeDao();
+}
